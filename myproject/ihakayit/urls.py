bc26d64d1cd812cf3c3b2eda66cfa7157b7be588
atahan <39523757+AtahanCelebi@users.noreply.github.com>
2023-02-23 00:47:13 +0300

    endpoint changes

--- myproject/ihakayit/urls.py
+++ myproject/ihakayit/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('add/', IHARegistrationView.as_view(),name='add-iha'),
-    path('list/', IHAListView.as_view(),name='get-list'),
-    path('search/', IHAFilterView.as_view(),name='search-iha'),
-    path('delete/<int:id>', IHADeleteView.as_view(), name='delete-iha'),
-    path('update/<int:id>', IHAUpdateView.as_view(), name='update-iha'),
+    path('ihas/register/', IHARegistrationView.as_view(),name='add-iha'),
+    path('ihas/list/', IHAListView.as_view(),name='get-list'),
+    path('ihas/filter/', IHAFilterView.as_view(),name='search-iha'),
+    path('ihas/<id>/delete/', IHADeleteView.as_view(), name='delete-iha'),
+    path('ihas/<id>/update/', IHAUpdateView.as_view(), name='update-iha'),
 ]
--- myproject/login/urls.py
+++ myproject/login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('api/user/register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('api/user/login/', LoginView.as_view(), name='login'),
+    path('api/user/logout/', LogoutView.as_view(), name='logout'),
 ]
